f6fb6feea0f77e6c2d4d6c4c1f44d9cd19da338f
dzchen <firewall2008dz@yahoo.com.cn>
2011-07-02 17:03:48 +0800

    modify

---
+++ xmpp/client/IQ.pas
@@ -0,0 +1,24 @@
+unit IQ;
+
+interface
+uses
+  Stanza,NativeXml,jid;
+type
+  TIqType=(iqget,
+		iqset,
+		iqresult,
+		iqerror);
+  TIQ=class(TStanza)
+  private
+    procedure FSetIqType(value:TIqType);
+    function FGetIqType:TIqType;
+  public
+    constructor Create(AOwner:TNativeXml);overload;
+    constructor Create(AOwner:TNativeXml;iqtype:TIqType);overload;
+    constructor Create(AOwner:TNativeXml;fromjid:TJID;tojid:TJID);overload;
+    constructor Create(AOwner:TNativeXml;iqtype:TIqType;fromjid:TJID;tojid:TJID);overload;
+    property IqType: TIqType read FGetIqType write FSetIqType;
+  end;
+implementation
+
+end.
